f29997a0e4438e17e51204e6c0f76c554b137a7c
Forrest Nelson <forrestnnelson@gmail.com>
2023-07-23 14:28:06 -0500

    appending and updating calc answer on DOM functional

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -8,6 +8,8 @@ function onReady() {
     $('.operator-btn').on('click', handleOperator)
     $('#equals-button').on('click', handleEquals);
 
+    // this function added here so that when we refresh the page the history still shows on the DOM
+    updateCalcHistory();
 }
 
 function handleOperator(event) {
@@ -60,6 +62,7 @@ function updateCalcHistory() {
     }).then((response) => {
         calcHistory = response;
         console.log('in GET calc history:', calcHistory)
+        render(calcHistory);
     }).catch((error) => {
         console.log('error caught:', error);
         alert('ERROR on GET')
@@ -68,3 +71,28 @@ function updateCalcHistory() {
 
 
 // Note: when updating current answer on DOM, the answer from latest calc can be the array[0].answer
+
+function render(arrayOfObjects) {
+    $('#calc-history-list').empty();
+    for (let object of arrayOfObjects) {
+        // conditional to get 'addition' to show +, etc (still wondering if I could have kept operators as strings and still have them work)
+        if (object.operator == 'addition') {
+            object.operator = '+';
+        } else if (object.operator == 'subtraction') {
+            object.operator = '-';
+        } else if (object.operator == 'multiplication') {
+            object.operator = '*';
+        } else if (object.operator == 'division') {
+            object.operator = '/';
+        }
+
+        // appending whole array (calc history) to DOM
+        $('#calc-history-list').append(`
+        <li>${object.num1} ${object.operator} ${object.num2} = ${object.answer}</li>
+    `)
+    }
+    // had to add conditional since error when empty array at startup of server on homepage (no array[0].answer yet)
+    if (arrayOfObjects[0]) {
+        $('#calc-answer').text(`${arrayOfObjects[0].answer}`)
+    }
+}
